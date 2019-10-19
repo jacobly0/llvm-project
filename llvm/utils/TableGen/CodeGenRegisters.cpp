801b5e7511c022f84cbabd180f04a0c97eadad27
Jacob Young <jacobly0@users.noreply.github.com>
2019-10-20 00:34:50 +0200

    [TableGen] [GlobalISel] Add better support for register operands.

--- llvm/utils/TableGen/CodeGenRegisters.cpp
+++ llvm/utils/TableGen/CodeGenRegisters.cpp
@@ -1013,7 +1013,8 @@ CodeGenRegisterClass::getMatchingSubClassWithSubRegs(
       SuperRegRCs.emplace_back(&RC);
   llvm::sort(SuperRegRCs, SizeOrder);
 
-  assert(SuperRegRCs.front() == BiggestSuperRegRC &&
+  assert(SuperRegRCs.front()->getMembers().size() ==
+             BiggestSuperRegRC->getMembers().size() &&
          "Biggest class wasn't first");
 
   // Find all the subreg classes and order them by size too.
