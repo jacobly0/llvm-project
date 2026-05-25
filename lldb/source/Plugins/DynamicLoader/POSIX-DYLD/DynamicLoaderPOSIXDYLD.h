95fe48ddb465f1bc55b82da5017941e8a4857e8a
Jacob Young <jacobly@ziglang.org>
2026-05-25 14:20:00 -0400

    tls

--- lldb/source/Plugins/DynamicLoader/POSIX-DYLD/DynamicLoaderPOSIXDYLD.h
+++ lldb/source/Plugins/DynamicLoader/POSIX-DYLD/DynamicLoaderPOSIXDYLD.h
@@ -93,6 +93,12 @@ protected:
   /// Contains the pointer to the interpret module, if loaded.
   std::weak_ptr<lldb_private::Module> m_interpreter_module;
 
+  /// Indicates whether the initial set of modules was reported added.
+  bool m_initial_modules_added;
+
+  /// Cache exec tls size.
+  lldb::addr_t m_tls_memsz;
+
   /// Returns true if the process is for a core file.
   bool IsCoreFile() const;
 
@@ -105,9 +111,6 @@ protected:
   static bool RendezvousBreakpointHit(
       void *baton, lldb_private::StoppointCallbackContext *context,
       lldb::user_id_t break_id, lldb::user_id_t break_loc_id);
-
-  /// Indicates whether the initial set of modules was reported added.
-  bool m_initial_modules_added;
 
   /// Helper method for RendezvousBreakpointHit.  Updates LLDB's current set
   /// of loaded modules.
